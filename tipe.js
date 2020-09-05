86d6495b4def5a2fe324e5f8cb2a669b5239d8e9
SybelBlue <lbc.dna@gmail.com>
2020-09-05 18:10:53 -0500

    completed tipe system

--- tipe.js
+++ tipe.js
@@ -6,31 +6,53 @@ class TipeProperty {
     }
 }
 
+class TipeMethod extends TipeProperty {
+    constructor(name, inTipe, outTipe, compute) {
+        super(name, inTipe, outTipe);
+        this.compute = compute;
+    }
+    
+    run(tipedValue) {
+        if (Tipe.of(tipedValue).name !== this.inTipe.name) {
+            throw new Error('mismatched in tipes!', tipedValue, this);
+        }
+        const out = this.compute(tipedValue);
+        if (Tipe.of(out).name !== this.outTipe.name) {
+            throw new Error('mismatched out tipes!', out, this);
+        }
+        return out;
+    }
+}
+
+class TipedValue {
+    constructor(tipe, defaults={}) {
+        this.tipe = tipe;
+        if (tipe.basic) {
+            this.value = defaults.value;
+        }
+        for (const key in tipe.properties) {
+            this[key] = defaults[key] || tipe.properties[key].outTipe.new();
+        }
+    }
+    
+    callMethod(methodName) {
+        const method = this.tipe.methods[methodName];
+        if (!method) {
+            throw new Error(`method ${methodName} does not exist on ${tipe.name}`);
+        }
+        return method.run(value);
+    }
+}
+
 class Tipe {
     static name = 'top';
     static properties = {};
+    static methods = {};
     static new() { console.log('unimplemented!') }
     // provide middle top
     static draw(tipe) { console.log('draw unimplemented for ' + name); }
     // provide middle top
     static drawShadow() { console.log('drawShadow unimplemented for ' + name); }
-
-    static newFactory(tipe) {
-        return function(defaults={}) {
-            let out = { tipe: tipe };
-            for (const key in tipe.properties) {
-                out[key] = defaults[key] || tipe.properties[key].tipe.new();
-            }
-            return out;
-        }
-    }
-
-    static of(value) {
-        if (typeof(value) === typeof(0)) {
-            return NumberTipe;
-        }
-        return value.tipe;
-    }
 
     static Stream(tipe) {
         return class InnerTipe extends Tipe {
@@ -47,11 +69,32 @@ class Tipe {
     }
 }
 
+class BooleanTipe extends Tipe {
+    static name = 'Boolean';
+    static basic = true;
+    static isBooleanTipe = true;
+    static methods = {
+        negate: new TipeMethod('negate', BooleanTipe, BooleanTipe, function(self) { return BooleanTipe.new(!self.value); }),
+    };
+    static new(value=false) { return new TipedValue(BooleanTipe, { value: value }); }
+    
+    static drawShadow() {
+        TextTipe.draw('True/False', Layers.Shadow);
+    }
+
+    static draw(bool) {
+        TextTipe.draw(bool ? 'True' : 'False');
+    }
+}
+
 class NumberTipe extends Tipe {
     static name = 'Number';
     static basic = true;
     static isNumberTipe = true;
-    static new() { return 0; }
+    static methods = {
+        absoluteValue: new TipeMethod('absoluteValue', NumberTipe, NumberTipe, function(self) { return NumberTipe.new(abs(self.value)); }),
+    }
+    static new(value=0) { return new TipedValue(NumberTipe, { value: value }); }
     static shadowTextWidth = null;
     static shadowText = '#';
 
@@ -80,12 +123,12 @@ class NumberTipe extends Tipe {
 
 class TextTipe extends Tipe {
     static name = 'Text';
-    static basic = true;
-    static new() {
-        let obj = new String();
-        obj.tipe = TextTipe;
-        return obj;
-    }
+    static methods = {
+        length: new TipeMethod('length', TextTipe, NumberTipe, function(self) { return NumberTipe.new(self.value.length); }),
+        firstLetter: new TipeMethod('firstLetter', TextTipe, TextTipe, function(self) { return TextTipe.new(self.value.substring(0, 1)); }),
+        firstWord: new TipeMethod('firstWord', TextTipe, TextTipe, function(self) { return TextTipe.new(self.value.split(' ')[0]); }),
+    };
+    static new(value='') { return new TipedValue(TextTipe, { value: value }); }
     static shadowTextWidth = null;
     static shadowText = 'text';
 
@@ -117,8 +160,8 @@ class ColorTipe extends Tipe {
         blue: new TipeProperty('blue', ColorTipe, NumberTipe),
         red: new TipeProperty('red', ColorTipe, NumberTipe),
     }
-    static new = Tipe.newFactory(ColorTipe);
-    static asP5Color(c) { return color(c.red, c.green, c.blue); }
+    static new() { return new TipedValue(ColorTipe); }
+    static asP5Color(c) { return color(c.red.value, c.green.value, c.blue.value); }
 
     static drawShadow() {
         ColorTipe.draw({red: 20, green: 20, blue: 200}, Layers.Shadow);
@@ -140,7 +183,7 @@ class IDCardTipe extends Tipe {
         age: new TipeProperty('age', IDCardTipe, NumberTipe),
         eyes: new TipeProperty('eyes', IDCardTipe, ColorTipe),
     }
-    static new = Tipe.newFactory(IDCardTipe);
+    static new() { return new TipedValue(IDCardTipe); }
 }
 
 class BallTipe extends Tipe {
@@ -149,7 +192,7 @@ class BallTipe extends Tipe {
         size: new TipeProperty('size', BallTipe, NumberTipe),
         color: new TipeProperty('color', BallTipe, ColorTipe),
     }
-    static new = Tipe.newFactory(BallTipe);
+    static new() { return new TipedValue(BallTipe); }
 
     // expects top center
     static drawShadow() {
