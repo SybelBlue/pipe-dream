ce5fa2ddfeb2af729fd3a5cfc417c919a7712af1
SybelBlue <lbc.dna@gmail.com>
2020-09-09 20:05:06 -0500

    split up machine.js

---
+++ machines/basic-machines.js
@@ -0,0 +1,35 @@
+class MapMachine extends StackedMachine {
+    constructor(key, inTipe) {
+        super(key, inTipe, color('#E8E288'), 'map');
+    }
+}
+
+class FilterMachine extends StackedMachine {
+    get outputTipe() { return this.inTipe; }
+    get finished() { 
+        const last = Array.last(this.methodStack);
+        return (last ? last.outTipe : this.inTipe).name === BooleanTipe.name;
+    }
+
+    constructor(key, inTipe) {
+        super(key, inTipe, color('#7dce82'), 'filter');
+    }
+
+    apply(tipedValue) {
+        const result = super.apply(tipedValue);
+        return result.value ? tipedValue : null;
+    }
+
+    draw() {
+        super.draw();
+
+        Renderer.push(this);
+        Renderer.translate(Machine.bodyIndent + Tipe.shapeMidline, this.height - MapMachine.tailHeight);
+        Renderer.newRenderable(Layers.Machine, () => {
+            noStroke();
+            fill(Editor.backgroundColor);
+            BooleanTipe.shapeOutline(0)
+        });
+        Renderer.pop(this);
+    }
+}
---
+++ machines/stacked-machine.js
@@ -0,0 +1,78 @@
+class StackedMachine extends Machine {
+    static tailHeight = 20;
+
+    get outputTipe() { 
+        const last = Array.last(this.methodStack);
+        return last ? last.outTipe : this.inTipe;
+    }
+    get height() { return Machine.bodyHeight + this.innerHeight + StackedMachine.tailHeight; }
+    get finished() { return true; }
+    get innerHeight() { return max(10, Array.sum(this.methodStack.map(m => m.height)) + (this.finished ? 0 : 20)); }
+
+    methodStack = [];
+
+    constructor(key, inTipe, color, text) {
+        super(key, inTipe, color, text);
+    }
+
+    draw() {
+        // draw body
+        super.draw();
+        
+        Renderer.newRenderable(Layers.Machine, () => {
+            noStroke();
+
+            // clean interior
+            fill(Editor.backgroundColor);
+            rect(0, Machine.bodyHeight, Machine.bodyIndent, this.innerHeight);
+
+            // draw arm
+            fill(this.color);
+            rect(0, Machine.bodyHeight, Machine.bodyIndent, this.innerHeight);
+
+            // draw tail
+            rect(0, this.height - StackedMachine.tailHeight, Machine.width, StackedMachine.tailHeight, 0, 10, 10, 10);
+        });
+        
+        // draw fragment stack
+        Renderer.push(this);
+
+        Renderer.translate(Machine.bodyIndent, Machine.bodyHeight);
+        this.drawFragmentStack();
+
+        Renderer.pop(this);
+    }
+
+    drawFragmentStack() {
+        Renderer.push(this);
+        Renderer.translate(Tipe.shapeMidline, 0);
+        this.inTipe.drawShape(this.color);
+        Renderer.pop(this);
+
+        Renderer.push(this);
+        let currentTipe = this.inTipe;
+        this.methodStack.forEach((method, index) => {
+            method.draw(() => {
+                editor.tray.loadOptionsFor(method.outTipe, this, index);
+                this.fragmentClicked(method, index);
+            });
+            currentTipe = method.outputTipe;
+            Renderer.translate(0, method.height);
+        })
+        Renderer.pop(this);
+    }
+
+    fragmentClicked() { console.log('click within stacked machine'); }
+
+    pushFragment(fragment, sourceIndex) { 
+        this.methodStack.splice(sourceIndex + 1, this.methodStack.length - sourceIndex - 1, fragment);
+        editor.tray.loadOptionsFor(fragment.outTipe, this, sourceIndex + 1);
+        editor.validatePipeline();
+    }
+
+    apply(tipedValue) { 
+        return this.methodStack.reduce(function (prev, method) {
+            return method.run(prev);
+        }, tipedValue);
+    }
+}
