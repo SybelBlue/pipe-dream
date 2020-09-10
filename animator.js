19b32a1a34a26a3b8c5b356fe82a93d61a99f708
SybelBlue <lbc.dna@gmail.com>
2020-09-10 09:39:39 -0500

    worked out animator bugs

--- animator.js
+++ animator.js
@@ -5,19 +5,18 @@ class Animator {
         update=function(last){ return last; },
         completed=function(next) { return false; },
         callback=function(){}) {
-        this.draw = drawFunction;
+        this.innerDraw = drawFunction;
         this.update = update;
         this.completed = completed;
         this.finished = false;
         this.callback = callback;
 
         this.nextValue = init;
-        this.checkCompleted();
     }
 
     checkCompleted() {
         if (this.completed(this.nextValue)) {
-            callback();
+            this.callback();
             this.finished = true;
         }
     }
@@ -27,24 +26,29 @@ class Animator {
 
         Renderer.push(this);
         Renderer.translate(this.nextValue[0], this.nextValue[1]);
-        this.draw();
+        this.innerDraw();
         Renderer.pop(this);
 
-        this.nextValue = this.update(this.nextValue);
         this.checkCompleted();
+        if (!this.finished) {
+            this.nextValue = this.update(this.nextValue);
+        }
     }
 }
 
-class LerpAnimator {
+class LerpAnimator extends Animator {
     constructor(drawFunction, start, stop, speed, callback) {
-        this.dist = dist(...start, ...stop);
-        this.dir = [0, 1].map(i => stop[i] - start[i] / this.dist);
-        this.checkOps = [0, 1].map(i => start[i] <= stop[i] ? max : min);
         super(
             drawFunction,
             start,
-            (last) => [0, 1].map(i => this.checkOps[i](last[i] + this.dir[i] * speed, stop[i])),
-            (next) => next == stop,
+            (last) => {
+                this.dist = this.dist || dist(...start, ...stop);
+                this.dir = this.dir || [0, 1].map(i => (stop[i] - start[i]) / this.dist);
+                this.checkOps = this.checkOps || [0, 1].map(i => start[i] <= stop[i] ? min : max);
+
+                return [0, 1].map(i => this.checkOps[i](last[i] + this.dir[i] * speed, stop[i]));
+            },
+            curr => abs(curr[0] - stop[0]) <= 0.01 && abs(curr[1] - stop[1]) <= 0.01,
             callback
         );
     }
