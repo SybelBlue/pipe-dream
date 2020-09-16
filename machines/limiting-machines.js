6a1f0b789e4abc4b219a8c2b0c3fc7d8767e4ca4
SybelBlue <lbc.dna@gmail.com>
2020-09-15 20:24:27 -0500

    split off limiting machines

---
+++ machines/limiting-machines.js
@@ -0,0 +1,70 @@
+class TakeMachine extends Machine {
+    static inputBoxStart = Machine.bodyHeight;
+
+    finished = true;
+
+    get height() { return this.bodyHeight; }
+    get bodyHeight() { return TakeMachine.inputBoxStart + this.inputBox.height + 10; }
+
+    get outputTipe() { return this.inTipe; }
+
+    get closedPipeline() { return this.remaining <= 0; }
+
+    constructor(key, inTipe) {
+        super(key, inTipe, color('#38369A'), 'take');
+        this.inputBox = new IntegerBox({ defaultText: '3' });
+    }
+
+    draw() {
+        super.draw();
+        Renderer.temporary(this, StackedMachine.bodyIndent, TakeMachine.inputBoxStart, 
+            () => this.inputBox.draw(SceneManager.editable));
+    }
+
+    process(values) { return values.slice(0, this.inputBox.value); }
+
+    reset() { this.remaining = this.inputBox.value; }
+
+    accept(tipedValue) { 
+        if (this.remaining > 0) {
+            this.remaining--;
+            return tipedValue;
+        }
+        return null;
+    }
+}
+
+class DropMachine extends Machine {
+    static inputBoxStart = Machine.bodyHeight;
+
+    finished = true;
+
+    get height() { return this.bodyHeight; }
+
+    get bodyHeight() { return DropMachine.inputBoxStart + this.inputBox.height + 10; }
+
+    get outputTipe() { return this.inTipe; }
+
+    constructor(key, inTipe) {
+        super(key, inTipe, color('#48A9A6'), 'drop');
+        this.inputBox = new IntegerBox({ defaultText: '3' });
+    }
+
+    draw() {
+        super.draw();
+        Renderer.temporary(this, StackedMachine.bodyIndent, TakeMachine.inputBoxStart, 
+            () => this.inputBox.draw(SceneManager.editable));
+    }
+
+    process(values) { return values.slice(this.inputBox.value); }
+
+    reset() { this.remaining = this.inputBox.value; }
+
+    accept(tipedValue) { 
+        if (this.remaining > 0) {
+            this.remaining--;
+            return null;
+        }
+        return tipedValue;
+    }
+}
