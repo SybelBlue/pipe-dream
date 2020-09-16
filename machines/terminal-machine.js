649106da1702a1991655705c5e54e0c69319c9e9
SybelBlue <lbc.dna@gmail.com>
2020-09-16 16:27:03 -0500

    fixed whitespace, FirstMachine extends Terminal

--- machines/terminal-machine.js
+++ machines/terminal-machine.js
@@ -1,24 +1,24 @@
 class TerminalMachine extends Machine {
-        isTerminal = true;
-        greedy = true;
+    isTerminal = true;
+    greedy = true;
 
-        constructor(key, inTipe, color, text) {
-           super(key, inTipe, color, text);
-        }
-
-        draw() {
-            super.draw();
-        }
-
-        process(values) { return values[0]; }
-}
-
-class FirstMachine extends Machine {
     finished = true;
 
-    get outputTipe() { return this.inTipe; }
-
     closedPipeline = false;
+
+    constructor(key, inTipe, color, text) {
+        super(key, inTipe, color, text);
+    }
+
+    draw() {
+        super.draw();
+    }
+
+    process(values) { return values[0]; }
+}
+
+class FirstMachine extends TerminalMachine {
+    get outputTipe() { return this.inTipe; }
 
     constructor(key, inTipe) {
         super(key, inTipe, color('#3D5A80'), 'first');
