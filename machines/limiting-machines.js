83fbd152844227edc28e3e5ce94ecd878a3a1f7f
SybelBlue <lbc.dna@gmail.com>
2020-09-16 16:19:21 -0500

    migrated FirstMachine into limiting-machines

--- machines/limiting-machines.js
+++ machines/limiting-machines.js
@@ -34,30 +34,6 @@ class TakeMachine extends Machine {
     }
 }
 
-class FirstMachine extends Machine {
-    finished = true;
-
-    get outputTipe() { return this.inTipe; }
-
-    closedPipeline = false;
-
-    constructor(key, inTipe) {
-        super(key, inTipe, color('#3D5A80'), 'first');
-    }
-
-    process(values) { return values.slice(0, 1); }
-
-    reset() { this.closedPipeline = false; }
-
-    accept(tipedValue) { 
-        if (this.closedPipeline) {
-            return null;
-        }
-        this.closedPipeline = true;
-        return tipedValue;
-    }
-}
-
 class DropMachine extends Machine {
     static inputBoxStart = Machine.bodyHeight;
 
--- machines/terminal-machine.js
+++ machines/terminal-machine.js
@@ -1,3 +1,38 @@
 class TerminalMachine extends Machine {
-    isTerminal = true;
+        isTerminal = true;
+        greedy = true;
+
+        constructor(key, inTipe, color, text) {
+           super(key, inTipe, color, text);
+        }
+
+        draw() {
+            super.draw();
+        }
+
+        process(values) { return values[0]; }
+}
+
+class FirstMachine extends Machine {
+    finished = true;
+
+    get outputTipe() { return this.inTipe; }
+
+    closedPipeline = false;
+
+    constructor(key, inTipe) {
+        super(key, inTipe, color('#3D5A80'), 'first');
+    }
+
+    process(values) { return values.slice(0, 1); }
+
+    reset() { this.closedPipeline = false; }
+
+    accept(tipedValue) { 
+        if (this.closedPipeline) {
+            return null;
+        }
+        this.closedPipeline = true;
+        return tipedValue;
+    }
 }
