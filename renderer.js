276eba5cd833a94025e4eb222796727c8e4bf345
SybelBlue <lbc.dna@gmail.com>
2020-09-24 11:11:48 -0500

    made Renderer a const obj

--- renderer.js
+++ renderer.js
@@ -11,92 +11,27 @@ const Layers = {
     Debug: 99,
 };
 
-class Renderer {
-    static Node = class {
-        static get Head() { return new Renderer.Node(0, null, 'Renderer Head'); }
-        constructor(key, previous, source) {
-            this.key = key;
-            this.previous = previous;
-            this.source = source;
-            this.x = previous ? previous.x : 0;
-            this.y = previous ? previous.y : 0;
-        }
-        clone() {
-            return new Renderer.Node(this.key, this.previous, this.source);
-        }
-    }
-
-    static Region = class {
-        hovering = false;
-        clicked = false;
-
-        constructor(layer, x, y, width, height, blocking) {
-            exists(this.layer = layer, true);
-            exists(this.x = x, true);
-            exists(this.y = y, true);
-            exists(this.width = width, true);
-            exists(this.height = height, true);
-            exists(this.blocking = blocking, true);
-        }
-
-        test(x, y) {
-            return this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height;
-        }
-    }
-
-    static Renderable = class {
-        constructor(layer, draw, translation, regions) {
-            this.layer = layer;
-            this.draw = draw;
-            this.translation = translation;
-            this.regions = regions;
-        }
-
-        static from(layer, drawCallback, regionStubs) {
-            const regions = {};
-
-            regionStubs.forEach((stub) => { 
-                const region = new Renderer.Region(
-                    layer, 
-                    Renderer.xTranslation + stub.x, 
-                    Renderer.yTranslation + stub.y, 
-                    stub.width, 
-                    stub.height, 
-                    stub.blocking
-                );
-                Renderer.registerRegion(region);
-                regions[stub.name] = region;
-            });
-
-            return new this(layer, drawCallback, [Renderer.xTranslation, Renderer.yTranslation], regions);
-        }
-    }
-
+const Renderer = {
     // must be monospaced!
-    static defaultFont = 'Courier New';
+    defaultFont: 'Courier New',
 
-    static _keyCount = 1;
-    static stackTop = Renderer.Node.Head.clone();
-    static toRender = [];
-    static regions = [];
+    _keyCount: 1,
+    toRender: [],
+    regions: [],
 
-    static keyListeners = [];
+    keyListeners: [],
 
-    static clearStack() { 
-        if (Renderer.stackTop.key !== Renderer.Node.Head.key) {
-            console.warn('cleared while non-empty render stack');
-            Renderer.stackTop = Renderer.Node.Head.clone();
-        }
-    }
+    textBoundMemoized: {},
 
-    static get yTranslation() { return Renderer.stackTop.y; }
+    // defined after Renderer.Node is defined at the end of the class
+    // stackTop: Renderer.Node.Head.clone(),
 
-    static get xTranslation() { return Renderer.stackTop.x; }
+    get yTranslation() { return Renderer.stackTop.y; },
 
-    static textBoundMemoized = {};
+    get xTranslation() { return Renderer.stackTop.x; },
 
     // assumes monospaced font!
-    static textWidth(text, size, font=this.defaultFont) {
+    textWidth(text, size, font=this.defaultFont) {
         const key = `w_${font}_${text.length}_${size}`;
         if (!exists(this.textBoundMemoized[key])) {
             push();
@@ -106,9 +41,9 @@ class Renderer {
             pop();
         }
         return this.textBoundMemoized[key];
-    }
+    },
 
-    static textHeight(size, font=this.defaultFont) {
+    textHeight(size, font=this.defaultFont) {
         const key = `h_${font}_${size}`;
         if (!exists(this.textBoundMemoized[key])) {
             push();
@@ -118,10 +53,10 @@ class Renderer {
             pop();
         }
         return this.textBoundMemoized[key];
-    }
+    },
 
     // assumes monospaced font!
-    static textToLines(rawText, textSize, maxWidth, font=this.defaultFont) {
+    textToLines(rawText, textSize, maxWidth, font=this.defaultFont) {
         const charWidth = Renderer.textWidth(' ', textSize, font);
         const charsInLine = floor(maxWidth / charWidth);
         if (charsInLine <= 1) return null;
@@ -155,16 +90,23 @@ class Renderer {
             output.push(current);
             return output;
         }, []);
-    }
+    },
 
-    static translate(x, y) {
+    clearStack() { 
+        if (!exists(Renderer.stackTop) || Renderer.stackTop.key !== Renderer.Node.Head.key) {
+            console.warn('cleared while non-empty render stack');
+            Renderer.stackTop = Renderer.Node.Head.clone();
+        }
+    },
+
+    translate(x, y) {
         if (typeof(x) !== typeof(0) || Number.isNaN(x)) throw new Error('Renderer.translate was expecting a numeric first argument');
         if (typeof(y) !== typeof(0) || Number.isNaN(y)) throw new Error('Renderer.translate was expecting a numeric second argument');
         Renderer.stackTop.x += x;
         Renderer.stackTop.y += y;
-    }
+    },
 
-    static push(source) {
+    push(source) {
         if (source == null) throw new Error('null source!');
 
         const key = Renderer._keyCount++;
@@ -172,24 +114,24 @@ class Renderer {
         Renderer.stackTop = new Renderer.Node(key, Renderer.stackTop, source);
 
         // return function () { Renderer.pop(key); } // make pop take key?
-    }
+    },
 
-    static pop(source) {
+    pop(source) {
         if (source !== Renderer.stackTop.source || Renderer.stackTop.key == 0) {
             throw new Error('Unexpected Pop from ' + source);
         }
 
         Renderer.stackTop = Renderer.stackTop.previous;
-    }
+    },
 
-    static temporary(source, xTranslation, yTranslation, callback) {
+    temporary(source, xTranslation, yTranslation, callback) {
         Renderer.push(source);
         Renderer.translate(xTranslation, yTranslation);
         callback();
         Renderer.pop(source);
-    }
+    },
 
-    static regionStub(name, x, y, width, height, blocking=true) {
+    regionStub(name, x, y, width, height, blocking=true) {
         return {
             name: name,
             x: x,
@@ -198,18 +140,18 @@ class Renderer {
             height: height,
             blocking: blocking
         };
-    }
+    },
 
-    static newRenderable(layer, drawCallback, ...regionStubs) {
+    newRenderable(layer, drawCallback, ...regionStubs) {
         let i = 0;
         while (i < Renderer.toRender.length && Renderer.toRender[i].layer <= layer) {
             i++;
         }
 
         Renderer.toRender.splice(i, 0, Renderer.Renderable.from(layer, drawCallback, regionStubs));
-    }
+    },
 
-    static newUIButton(txt, textColor, onClick, margin=10, fontSize=24) {
+    newUIButton(txt, textColor, onClick, margin=10, fontSize=24) {
         const tHeight = Renderer.textHeight(fontSize);
         const height = tHeight + 2 * margin;
         const width = Renderer.textWidth(txt, fontSize) + 2 * margin;
@@ -225,18 +167,18 @@ class Renderer {
             if (regions.button.clicked) onClick();
         }, Renderer.regionStub('button', 0, 0, width, height));
         return { width: width, height: height };
-    }
+    },
 
-    static registerRegion(region) {
+    registerRegion(region) {
         let i = 0;
         while (i < Renderer.regions.length && Renderer.regions[i].layer > region.layer) {
             i++;
         }
 
         Renderer.regions.splice(i, 0, region);
-    }
+    },
 
-    static recomputeRegions() {
+    recomputeRegions() {
         return Renderer.regions.reduce(function(results, region) {
             region.hovering = !results.found && region.test(mouseX, mouseY);
 
@@ -247,9 +189,9 @@ class Renderer {
 
             return results;
         }, { found: false, intercepted: true });
-    }
+    },
 
-    static renderAll() {
+    renderAll() {
         const hit = Renderer.recomputeRegions();
 
         push();
@@ -268,5 +210,68 @@ class Renderer {
         Renderer._keyCount = 1;
 
         return hit;
+    }
+}
+
+
+Renderer.Node = class {
+    static get Head() { return new Renderer.Node(0, null, 'Renderer Head'); }
+    constructor(key, previous, source) {
+        this.key = key;
+        this.previous = previous;
+        this.source = source;
+        this.x = previous ? previous.x : 0;
+        this.y = previous ? previous.y : 0;
+    }
+    clone() {
+        return new Renderer.Node(this.key, this.previous, this.source);
+    }
+}
+
+Renderer.stackTop = Renderer.Node.Head.clone();
+
+Renderer.Region = class {
+    hovering = false;
+    clicked = false;
+
+    constructor(layer, x, y, width, height, blocking) {
+        exists(this.layer = layer, true);
+        exists(this.x = x, true);
+        exists(this.y = y, true);
+        exists(this.width = width, true);
+        exists(this.height = height, true);
+        exists(this.blocking = blocking, true);
+    }
+
+    test(x, y) {
+        return this.x <= x && x <= this.x + this.width && this.y <= y && y <= this.y + this.height;
+    }
+}
+
+Renderer.Renderable = class {
+    constructor(layer, draw, translation, regions) {
+        this.layer = layer;
+        this.draw = draw;
+        this.translation = translation;
+        this.regions = regions;
+    }
+
+    static from(layer, drawCallback, regionStubs) {
+        const regions = {};
+
+        regionStubs.forEach((stub) => { 
+            const region = new Renderer.Region(
+                layer, 
+                Renderer.xTranslation + stub.x, 
+                Renderer.yTranslation + stub.y, 
+                stub.width, 
+                stub.height, 
+                stub.blocking
+            );
+            Renderer.registerRegion(region);
+            regions[stub.name] = region;
+        });
+
+        return new Renderer.Renderable(layer, drawCallback, [Renderer.xTranslation, Renderer.yTranslation], regions);
     }
 }
